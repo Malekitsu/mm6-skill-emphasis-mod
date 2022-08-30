110ae4f32cdb90b97c942f545ac0c8db075f6ef2
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2022-08-30 20:14:18 +0200

    0.8.4
    Timothy Bell upgrade
    Removed body building link
    Adaptive monster disabled
    Mana regen set to mana^0.5*rank^1.5/100

---
+++ Scripts/General/meditation-sp-regen.lua
@@ -0,0 +1,16 @@
+-- Add a bit of sp regeneration by meditation skill
+
+function calculateMeditationSPRegen(mastery, fullSP)
+	output = math.ceil(fullSP^0.5 * mastery^1.5/100)
+	return math.max(output, mastery)
+end
+
+function events.Regeneration(t)
+	v = Party[t.PlayerIndex]
+	
+	r,m = SplitSkill(v.Skills[const.Skills.Meditation])
+	cap = v:GetFullSP()
+	cur = v.SpellPoints
+	gain = t.SP + calculateMeditationSPRegen(m, cap)
+	v.SpellPoints = math.min(cap,cur+gain)
+end
--- Scripts/General/skem-monster-move.lua
+++ Scripts/General/skem-monster-move.lua
@@ -5,8 +5,12 @@
 --
 
 function monsterSubstitutions()
+	Game.MapStats[7].Monster3Pic = "Ogre"
+	Game.MapStats[7].Monster2Pic = "Archer"
 	Game.MapStats[36].Monster1Pic = "Monk"
+	Game.MapStats[45].Monster3Pic = "KnightPlate"
 	Game.MapStats[47].Monster2Pic = "Monk"
+	
 end
 
 function events.GameInitialized2()
