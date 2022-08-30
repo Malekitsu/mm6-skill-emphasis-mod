c754156b6de5282058e207d717ba8f504addab6d
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2022-08-30 21:51:43 +0200

    Update meditation-sp-regen.lua

--- Scripts/General/meditation-sp-regen.lua
+++ Scripts/General/meditation-sp-regen.lua
@@ -1,7 +1,7 @@
 -- Add a bit of sp regeneration by meditation skill
 
 function calculateMeditationSPRegen(mastery, fullSP)
-	output = math.ceil(fullSP^0.5 * mastery^1.5/100)
+	output = math.ceil(fullSP^0.5 * mastery^1.5/50)
 	return math.max(output, mastery)
 end
 
